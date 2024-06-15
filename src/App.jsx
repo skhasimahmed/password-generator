aeed3e56b5ac5ecab1d6e7475d2a7b52b8b68861
Sekh Hasim Ahmed <hasim.cs@gmail.com>
2024-06-15 17:28:48 +0530

    Update Styling.

--- src/App.jsx
+++ src/App.jsx
@@ -34,63 +34,87 @@ export default function App() {
   const { password, errorMessage, generatePassword } = usePasswordGenerator();
 
   return (
-    <div className="container">
-      {/* Password Text and Copy */}
-      {password && (
-        <div className="header">
-          <div className="title">{password}</div>
-          <Button
-            text={copied ? "Copied" : "copy"}
-            onClick={handleCopy}
-            customClass="copyBtn"
+    <div className="App">
+      <div className="container">
+        <div
+          style={{
+            color: "white",
+            position: "relative",
+            textAlign: "center",
+            fontSize: "28px",
+            marginBottom: "50px",
+          }}
+        >
+          <h1>
+            <span
+              style={{
+                color: "black",
+              }}
+            >
+              Fast
+            </span>
+            <span style={{ color: "red" }}>
+              Pass...|<sup>&reg;</sup>
+            </span>
+          </h1>
+        </div>
+        {/* Password Text and Copy */}
+        {password && (
+          <div className="header">
+            <div className="title">{password}</div>
+            <Button
+              text={copied ? "Copied" : "copy"}
+              onClick={handleCopy}
+              customClass="copyBtn"
+            />
+          </div>
+        )}
+
+        {/* Character Length */}
+        <div className="charlength">
+          <span>
+            <label>Character Length</label>
+            <label>{length}</label>
+          </span>
+          <input
+            type="range"
+            min="2"
+            max="25"
+            value={length}
+            onChange={(e) => {
+              setLength(e.target.value);
+              generatePassword(checkboxData, e.target.value);
+            }}
           />
         </div>
-      )}
 
-      {/* Character Length */}
-      <div className="charlength">
-        <span>
-          <label>Character Length</label>
-          <label>{length}</label>
-        </span>
-        <input
-          type="range"
-          min="4"
-          max="25"
-          value={length}
-          onChange={(e) => {
-            setLength(e.target.value);
-            generatePassword(checkboxData, e.target.value);
-          }}
+        {/* Checkboxes */}
+        <div className="checkboxes">
+          {checkboxData.map((checkbox, index) => {
+            return (
+              <Checkbox
+                key={index}
+                title={checkbox.title}
+                onChange={() => handleCheckboxChange(index)}
+                state={checkbox.state}
+              />
+            );
+          })}
+        </div>
+
+        {/* Strength */}
+        <PasswordStrengthIndicator password={password} />
+
+        {/* Error Handling */}
+        {errorMessage && <div className="errorMessage">{errorMessage}</div>}
+
+        {/* Generate Button */}
+        <Button
+          text="Generate Password"
+          onClick={() => generatePassword(checkboxData, length)}
+          customClass="generateBtn"
         />
       </div>
-
-      {/* Checkboxes */}
-      <div className="checkboxes">
-        {checkboxData.map((checkbox, index) => {
-          return (
-            <Checkbox
-              key={index}
-              title={checkbox.title}
-              onChange={() => handleCheckboxChange(index)}
-              state={checkbox.state}
-            />
-          );
-        })}
-      </div>
-
-      {/* Strength */}
-      <PasswordStrengthIndicator password={password} />
-
-      {/* Error Handling */}
-      {errorMessage && <div className="errorMessage">{errorMessage}</div>}
-
-      {/* Generate Button */}
-      <Button
-        text="Generate Password"
-        onClick={() => generatePassword(checkboxData, length)}
-        customClass="generateBtn"
-      />
     </div>
   );
 }
--- src/components/StrengthChecker.jsx
+++ src/components/StrengthChecker.jsx
@@ -4,15 +4,15 @@ const PasswordStrengthIndicator = ({ password = "" }) => {
   const getPasswordStrength = () => {
     const passwordLength = password.length;
 
-    if (passwordLength < 1) {
-      return "";
-    } else if (passwordLength < 4) {
+    if (passwordLength < 4) {
       return "Very Weak";
     } else if (passwordLength < 8) {
-      return "Poor";
+      return "Weak";
     } else if (passwordLength < 12) {
-      return "Medium";
+      return "Poor";
     } else if (passwordLength < 16) {
+      return "Medium";
+    } else if (passwordLength < 20) {
       return "Strong";
     } else {
       return "Very Strong";
@@ -21,11 +21,30 @@ const PasswordStrengthIndicator = ({ password = "" }) => {
 
   const passwordStrength = getPasswordStrength();
 
+  let strengthColor = "";
+
+  if (passwordStrength === "Very Weak") {
+    strengthColor = "#FF0000";
+  } else if (passwordStrength === "Weak") {
+    strengthColor = "#DC143C";
+  } else if (passwordStrength === "Poor") {
+    strengthColor = "#FF8C00";
+  } else if (passwordStrength === "Medium") {
+    strengthColor = "#B8860B";
+  } else if (passwordStrength === "Strong") {
+    strengthColor = "#32CD32";
+  } else if (passwordStrength === "Very Strong") {
+    strengthColor = "#228B22";
+  }
+
   if (!passwordStrength) return <></>;
 
   return (
     <div className="password-strength">
-      Strength: <span style={{ fontWeight: "bold" }}>{passwordStrength}</span>
+      Strength:{" "}
+      <span style={{ fontWeight: "bold", color: strengthColor }}>
+        {passwordStrength}
+      </span>
     </div>
   );
 };
